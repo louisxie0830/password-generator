892f77c265513e3878a3d290ffbec75dbe46ed9f
Nil Xie <bfgh2678@gmail.com>
2024-06-04 10:18:00 +0800

    fix: clean code

--- app/page.tsx
+++ app/page.tsx
@@ -2,73 +2,42 @@
 
 import { useCallback, useRef, useState } from 'react';
 import getRandomCharacter from '@/utils/generatorPassword';
+
 export default function Home() {
   const [password, setPassword] = useState('');
-  const [lowercaseIsChecked, setLowercaseIsChecked] = useState(true);
-  const [uppercaseIsChecked, setUppercaseIsChecked] = useState(false);
-  const [numbersIsChecked, setNumbersIsChecked] = useState(true);
-  const [symbolsIsChecked, setSymbolsIsChecked] = useState(true);
-  const [excDuplicateIsChecked, setExcDuplicateIsChecked] = useState(false);
-  const [spacesIsChecked, setSpacesIsChecked] = useState(false);
+  const [settings, setSettings] = useState({
+    lowercase: true,
+    uppercase: false,
+    numbers: true,
+    symbols: true,
+    excludeDuplicate: false,
+    spaces: false,
+  });
   const [passwordLength, setPasswordLength] = useState(12);
   const [copyType, setCopyType] = useState('Copy');
   const inputRef = useRef<HTMLInputElement>(null);
 
   const handlePasswordLengthChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
-      if (event.target.value === '') {
-        setPasswordLength(0);
-      } else {
-        setPasswordLength(parseInt(event.target.value, 10));
-      }
+      setPasswordLength(
+        event.target.value ? parseInt(event.target.value, 10) : 0,
+      );
     },
     [],
   );
 
-  const handleLowercaseChange = useCallback(
+  const handleCheckboxChange = useCallback(
     (event: React.ChangeEvent<HTMLInputElement>) => {
-      setLowercaseIsChecked(event.target.checked);
-    },
-    [],
-  );
-
-  const handleUppercaseChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      setUppercaseIsChecked(event.target.checked);
-    },
-    [],
-  );
-
-  const handleNumbersChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      setNumbersIsChecked(event.target.checked);
-    },
-    [],
-  );
-
-  const handleSymbolsChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      setSymbolsIsChecked(event.target.checked);
-    },
-    [],
-  );
-
-  const handleExcDuplicateChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      setExcDuplicateIsChecked(event.target.checked);
-    },
-    [],
-  );
-
-  const handleSpacesChange = useCallback(
-    (event: React.ChangeEvent<HTMLInputElement>) => {
-      setSpacesIsChecked(event.target.checked);
+      const { name, checked } = event.target;
+      setSettings((prevSettings) => ({
+        ...prevSettings,
+        [name]: checked,
+      }));
     },
     [],
   );
 
   const copyToClipboard = useCallback(async () => {
-    debugger;
     if (inputRef.current) {
       inputRef.current.select();
       try {
@@ -84,6 +53,8 @@ export default function Home() {
   }, []);
 
   const generatePassword = useCallback(() => {
+    const { lowercase, uppercase, numbers, symbols, excludeDuplicate, spaces } =
+      settings;
     const lowercaseChars = 'abcdefghijklmnopqrstuvwxyz';
     const uppercaseChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const numberChars = '0123456789';
@@ -91,11 +62,11 @@ export default function Home() {
     const spaceChar = ' ';
 
     let characters = '';
-    if (lowercaseIsChecked) characters += lowercaseChars;
-    if (uppercaseIsChecked) characters += uppercaseChars;
-    if (numbersIsChecked) characters += numberChars;
-    if (symbolsIsChecked) characters += symbolChars;
-    if (spacesIsChecked) characters += spaceChar;
+    if (lowercase) characters += lowercaseChars;
+    if (uppercase) characters += uppercaseChars;
+    if (numbers) characters += numberChars;
+    if (symbols) characters += symbolChars;
+    if (spaces) characters += spaceChar;
 
     if (characters === '') {
       setPassword('');
@@ -104,20 +75,12 @@ export default function Home() {
 
     let generatorPassword = '';
     while (generatorPassword.length < passwordLength) {
-      let char = getRandomCharacter(characters);
-      if (excDuplicateIsChecked && generatorPassword.includes(char)) continue;
+      const char = getRandomCharacter(characters);
+      if (excludeDuplicate && generatorPassword.includes(char)) continue;
       generatorPassword += char;
     }
     setPassword(generatorPassword);
-  }, [
-    lowercaseIsChecked,
-    uppercaseIsChecked,
-    numbersIsChecked,
-    symbolsIsChecked,
-    excDuplicateIsChecked,
-    spacesIsChecked,
-    passwordLength,
-  ]);
+  }, [settings, passwordLength]);
 
   return (
     <div className="container">
@@ -130,7 +93,7 @@ export default function Home() {
             name="password"
             id="password"
             value={password}
-            readOnly={true}
+            readOnly
           />
           <span id="copy" onClick={copyToClipboard}>
             {copyType}
@@ -148,60 +111,49 @@ export default function Home() {
                 onChange={handlePasswordLengthChange}
               />
             </li>
-            <li className="option">
-              <input
-                id="lowercase"
-                type="checkbox"
-                checked={lowercaseIsChecked}
-                onChange={handleLowercaseChange}
-              />
-              <label htmlFor="lowercase">Lowercase (a-z)</label>
-            </li>
-            <li className="option">
-              <input
-                id="uppercase"
-                type="checkbox"
-                checked={uppercaseIsChecked}
-                onChange={handleUppercaseChange}
-              />
-              <label htmlFor="uppercase">Uppercase (A-Z)</label>
-            </li>
-            <li className="option">
-              <input
-                id="numbers"
-                type="checkbox"
-                checked={numbersIsChecked}
-                onChange={handleNumbersChange}
-              />
-              <label htmlFor="numbers">Numbers (0-9)</label>
-            </li>
-            <li className="option">
-              <input
-                id="symbols"
-                type="checkbox"
-                checked={symbolsIsChecked}
-                onChange={handleSymbolsChange}
-              />
-              <label htmlFor="symbols">Symbols (!-$^+)</label>
-            </li>
-            <li className="option">
-              <input
-                id="exc-duplicate"
-                type="checkbox"
-                checked={excDuplicateIsChecked}
-                onChange={handleExcDuplicateChange}
-              />
-              <label htmlFor="exc-duplicate">Exclude Duplicate</label>
-            </li>
-            <li className="option">
-              <input
-                id="spaces"
-                type="checkbox"
-                checked={spacesIsChecked}
-                onChange={handleSpacesChange}
-              />
-              <label htmlFor="spaces">Include Spaces</label>
-            </li>
+            {[
+              {
+                id: 'lowercase',
+                label: 'Lowercase (a-z)',
+                checked: settings.lowercase,
+              },
+              {
+                id: 'uppercase',
+                label: 'Uppercase (A-Z)',
+                checked: settings.uppercase,
+              },
+              {
+                id: 'numbers',
+                label: 'Numbers (0-9)',
+                checked: settings.numbers,
+              },
+              {
+                id: 'symbols',
+                label: 'Symbols (!-$^+)',
+                checked: settings.symbols,
+              },
+              {
+                id: 'excludeDuplicate',
+                label: 'Exclude Duplicate',
+                checked: settings.excludeDuplicate,
+              },
+              {
+                id: 'spaces',
+                label: 'Include Spaces',
+                checked: settings.spaces,
+              },
+            ].map(({ id, label, checked }) => (
+              <li key={id} className="option">
+                <input
+                  id={id}
+                  type="checkbox"
+                  name={id}
+                  checked={checked}
+                  onChange={handleCheckboxChange}
+                />
+                <label htmlFor={id}>{label}</label>
+              </li>
+            ))}
           </ul>
         </div>
         <button
